658d5cbf1be41d53584ecb11251a6b522eb9b277
Alexander Traverso <nanaki1984@gmail.com>
2022-06-22 12:06:43 +0200

    notes on strings table

--- rl_core/src/strings_table.rs
+++ rl_core/src/strings_table.rs
@@ -7,7 +7,10 @@ use std::mem::MaybeUninit;
 use crate::fast_hash::{self, FastHash, SetKey};
 use crate::raw_set::RawSet;
 
-static mut STRINGS_TABLE: StringsTable = StringsTable::new();
+static mut STRINGS_TABLE: StringsTable = StringsTable::new(); // TODO multithreaded access (just use a mutex...)
+// would also be nice to have a const new on StringAtom using a 'static &str and keeping that in the entry (no allocation...but gets a lot more complicated)
+// const new for StringAtom is a must because of the mutex to be added (i can keep const StringAtoms around to not create them @ runtime)
+// The mutex in unreal is on shards, not on the whole table (every shard is a table, e.g. shards can be last 4 bits of hash, 16 shards)
 
 const STRINGS_TABLE_ENTRY_MAX_LEN: usize = 128;
 
