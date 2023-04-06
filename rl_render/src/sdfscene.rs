0ffb9476f8e8f05ad1a0a0280ad2086aafadea9e
Alexander Traverso <nanaki1984@gmail.com>
2023-04-06 10:07:47 +0200

    rust_analyzer works again

--- rl_render/src/sdfscene.rs
+++ rl_render/src/sdfscene.rs
@@ -1,14 +1,19 @@
 use nalgebra_glm::{Mat3x4};
 
 use rl_core::Array;
+use rl_math::AABB;
 use crate::SDFShape;
 
 struct SDFSceneEntry {
     shape: SDFShape,
+
     xform: Mat3x4,
     inv_xform: Mat3x4,
-    distance_scaling_factor: f32,
+    aabb: AABB,
+
     group_id: u32,
+
+    distance_scaling_factor: f32,
 }
 
 pub struct SDFScene {
